2a3080db3787d6f1d8468cc5f76b72d7a21d66b5
hannsi <hannsi.to2@gmail.com>
2026-05-12 16:30:28 +0900

    update

---
+++ R06/pre/Rectangle.java
@@ -0,0 +1,32 @@
+package pre;
+
+public class Rectangle {
+    private int xPosition;
+    private int yPosition;
+    private int width;
+    private int height;
+
+    public Rectangle(int xPosition, int yPosition, int width, int height) {
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
+        this.width = width;
+        this.height = height;
+    }
+
+    public void move(int xMove,int yMove){
+        xPosition = xPosition + xMove;
+        yPosition = yPosition + yMove;
+    }
+
+    public void showState(){
+        System.out.print("x座標: " + xPosition + ", y座標: " + yPosition);
+        System.out.println(", 幅: " + width + ", 高さ: " + height);
+    }
+
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(2,1,5,6);
+        rectangle.showState();
+        rectangle.move(2,2);
+        rectangle.showState();
+    }
+}
